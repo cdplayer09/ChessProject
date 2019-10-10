fe85db07300fac7e9e21514c59fbf5a3d304e487
cdplayer09 <Ciarandepree123@hotmail.com>
2019-10-10 18:41:45 +0100

    Bishop is working perfectly.
    Queen, rook and King to do.

--- ChessProject.java
+++ ChessProject.java
@@ -234,23 +234,23 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 			validMove = true;
 		}
 
-		else if(pieceName.equals("WhiteBishop")){
+		else if(pieceName.contains("Bishop")){
 			boolean inTheWay = false;
-			int distance = Math.abs(startX - landingY);/*This is for side movements*/
+			int distance = Math.abs(startX - landingX);/*This is for side movements*/
 			if(((landingX < 0) || (landingX > 7)) || ((landingY < 0) || (landingY > 7))){
 				validMove = false;
 				}
 			else{
 				validMove = true;
 				if(Math.abs(startX - landingX) == Math.abs(startY - landingY)){
-					if((startX - landingX < 0)&& (startY - landingY < 0)){
+					if((startX - landingX < 0) && (startY - landingY < 0)){
 						for (int i = 0; i < distance; i++){
 							if (piecePresent((initialX + (i *75)), (initialY + (i*75)))){
 								inTheWay = true;
 							}		
 						}
 					}
-					else if((startX - landingX < 0)&& (startY - landingY > 0)){
+					else if((startX - landingX < 0) && (startY - landingY > 0)){
 						for (int i = 0; i < distance; i++){
 							if (piecePresent((initialX + (i *75)), (initialY + (i*75)))){
 								inTheWay = true;
@@ -262,11 +262,48 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 							if (piecePresent((initialX + (i *75)), (initialY + (i*75)))){
 								inTheWay = true;
 							}	
+						}
+					}
+					else if((startX - landingY < 0) && (startY - landingY < 0)){
+						for (int i = 0; i < distance; i++){
+							if (piecePresent((initialX + (i *75)), (initialY + (i*75)))){
+								inTheWay = true;
+							}	
+						}
+					}
+					if(inTheWay){
+						validMove = false;
+					}
+					else{
+						if(piecePresent(e.getX(), e.getY())){
+							if(pieceName.contains("White")){
+								if(checkWhiteOponent(e.getX(), e.getY())){
+									validMove = true;
+								}
+								else{
+									validMove = false;
+								}
+							}
+							else{
+								if(checkBlackOponent(e.getX(), e.getY())){
+									validMove = true;
+								}
+								else{
+									validMove = false;
+								}
+							}
+						}
+						else{
+							validMove = true;
 						}
 					}
 				}
+				else{
+					validMove = false;
+				}
 			}
-		}
+		}//end of the bishop
+	
 		
 		else if (pieceName.equals("BlackKnight")){
 			if(((xMovement == 1)&&(yMovement==2))||((xMovement ==2) &&(yMovement == 1))){
